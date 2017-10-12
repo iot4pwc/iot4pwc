9ebd1729a5124fb2384dba1fcbc626be8c8cd577
Stefan Hermanek <hermanek.stefan@gmail.com>
2017-10-11 20:11:03 -0400

    IOT-67 Style updated
    Additionally included eclipse-java-google-style.xml in /other folder.
    Very helpful for all of us.
    Check
    http://www.practicesofmastery.com/post/eclipse-google-java-style-guide/
    for further info.

--- dummy_sensor/src/dummy_sensor/DummySensor.java
+++ dummy_sensor/src/dummy_sensor/DummySensor.java
@@ -22,56 +22,48 @@ import com.google.gson.Gson;
  */
 public class DummySensor {
 
-	public static void main(String[] args) 
-	{
-		final DummySensor ds = new DummySensor();
-		
-		Timer t = new Timer();
-		
-		// Every 5 seconds, make a POST to the given URL.
-		t.scheduleAtFixedRate(new TimerTask() {
-		    @Override
-		    public void run() 
-		    {
-		    	try
-				{
-					ds.makePOST(ds.generatePayload(), "https://postman-echo.com/post");
-				} catch (IOException e) 
-				{
-					e.printStackTrace();
-				}
-		    }
-		}, 1000,5000);
-		
-		
-	}
+  public static void main(String[] args) {
+    final DummySensor ds = new DummySensor();
 
-	// Generate the dummy payload.
-	private String generatePayload()
-	{
-		Map<String, Object> keyValuePairs = new HashMap<String, Object>();
-		keyValuePairs.put("sensorID", 1234);
-		keyValuePairs.put("sensorPayload", 96);
-		keyValuePairs.put("UTC", Instant.now().toEpochMilli());
-		
-		Gson gson = new Gson(); 
+    Timer t = new Timer();
 
-		return gson.toJson(keyValuePairs); 
-	}
-	
-	// Make a POST request with given requestBody and URL.
-	private void makePOST(String requestBody, String URL) throws IOException
-	{
-		URL url = new URL(URL);
-		HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-		httpCon.setDoOutput(true);
-		httpCon.setRequestMethod("POST");
-		OutputStreamWriter out = new OutputStreamWriter(
-				httpCon.getOutputStream());
-		System.out.println("POST " + URL + " " + requestBody);
-		out.write(requestBody);
-		System.out.println(httpCon.getResponseCode() + " " + httpCon.getResponseMessage());
-		out.close();
-	}
-	
+    // Every 5 seconds, make a POST to the given URL.
+    t.scheduleAtFixedRate(new TimerTask() {
+      @Override
+      public void run() {
+        try {
+          ds.makePOST(ds.generatePayload(), "https://postman-echo.com/post");
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }, 1000,5000);	
+  }
+
+  // Generate the dummy payload.
+  private String generatePayload() {
+    Map<String, Object> keyValuePairs = new HashMap<String, Object>();
+    keyValuePairs.put("sensorID", 1234);
+    keyValuePairs.put("sensorPayload", 96);
+    keyValuePairs.put("UTC", Instant.now().toEpochMilli());
+
+    Gson gson = new Gson(); 
+
+    return gson.toJson(keyValuePairs); 
+  }
+
+  // Make a POST request with given requestBody and URL.
+  private void makePOST(String requestBody, String URL) throws IOException {
+    URL url = new URL(URL);
+    HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+    httpCon.setDoOutput(true);
+    httpCon.setRequestMethod("POST");
+    OutputStreamWriter out = new OutputStreamWriter(
+        httpCon.getOutputStream());
+    System.out.println("POST " + URL + " " + requestBody);
+    out.write(requestBody);
+    System.out.println(httpCon.getResponseCode() + " " + httpCon.getResponseMessage());
+    out.close();
+  }
+
 }
