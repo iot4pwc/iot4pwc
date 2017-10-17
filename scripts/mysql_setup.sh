828fefdbf53ccb6aebfad87a7f6f3d04a9f82ecf
Eddie_Mac <edward.wuxianru@gmail.com>
2017-10-16 23:33:28 -0400

    remove useless files, fixed bug for mysql

--- scripts/mysql_setup.sh
+++ scripts/mysql_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.8-1_all.deb
-wget https://github.com/iot4pwc/iot4pwc/blob/master/service_platform_db/DB_DML.sql
+curl https://raw.githubusercontent.com/iot4pwc/iot4pwc/master/service_platform_db/DB_DML.sql > DB_DML.sql
 sudo dpkg -i mysql-apt-config*
 sudo apt-get update
-sudo apt-get install -y default-jre default-jdk maven mosquitto mysql-server
+sudo apt-get install -y mysql-server
 DB_USER_NAME="iot4pwc"
 export DB_USER_NAME
 DB_USER_PW="Heinz123!"
