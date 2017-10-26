45783605c152e115dc13fa1bae50c1cd178ad6f7
Eddie_Mac <edward.wuxianru@gmail.com>
2017-10-26 14:15:04 -0400

    added new tables, revised DBHelper and autonomized the whole scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,9 +5,9 @@ sudo apt-get install -y default-jre default-jdk maven mosquitto mosquitto-client
 sudo chmod 777 /etc/environment
 sudo echo "JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64/jre\"" >> /etc/environment
 source /etc/environment
-wget https://bintray.com/artifact/download/vertx/downloads/vert.x-3.4.2-full.tar.gz
-tar -zxf vert.x-3.4.2-full.tar.gz
-export PATH=~/vertx/bin:$PATH
+# wget https://bintray.com/artifact/download/vertx/downloads/vert.x-3.4.2-full.tar.gz
+# tar -zxf vert.x-3.4.2-full.tar.gz
+# export PATH=~/vertx/bin:$PATH
 HOST=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 export HOST
 DB_USER_NAME='iot4pwc'
@@ -15,3 +15,12 @@ export DB_USER_NAME
 DB_USER_PW='Heinz123!'
 export DB_USER_PW
 sudo ufw allow 37288
+echo "#################################################################################"
+echo "Please input the public ip and port of the MySQL instance: E.g 18.221.182.91:3306"
+read MYSQL_URL
+export MYSQL_URL
+echo "#################################################################################"
+echo "Please input the public DNS of the MQTT instance:"
+echo "E.g ec2-18-221-127-99.us-east-2.compute.amazonaws.com"
+read MQTT_URL
+export MQTT_URL
--- source/src/main/java/com/iot4pwc/constants/ConstLib.java
+++ source/src/main/java/com/iot4pwc/constants/ConstLib.java
@@ -22,19 +22,19 @@ public class ConstLib {
 
   public static final String SERVICE_PLATFORM_OPTION = "-sp";
   public static final String DATA_GENERATOR_OPTION = "-dg";
-  
-  public static final String MYSQL_CONNECTION_STRING = "jdbc:mysql://18.221.182.91:3306/service_platform?autoReconnect=true&useSSL=false";
+
+  public static final String MYSQL_CONNECTION_STRING = "jdbc:mysql://%s/%s?autoReconnect=true&useSSL=false";
+  public static final String SERVICE_PLATFORM = "service_platform";
+  public static final String INFOMATION_BROADCASTER = "information_broadcaster";
   public static final String HIKARI_POOL_NAME = "DBHelper connection pool";
   public static final int HIKARI_MAX_POOL_SIZE = 4;
   public static final boolean HIKARI_CACHE_PSTMT = true;
   public static final int HIKARI_PSTMT_CACHE_SIZE = 256;
   public static final boolean HIKARI_USE_SERVER_PSTMT = true;
 
-  public static final String MQTT_HOST = "localhost";
-  public static final int MQTT_PORT = 8883;
   public static final boolean MQTT_TLS_ENABLED = false;
-  public static final String MQTT_BROKER_STRING = "tcp://ec2-18-221-127-99.us-east-2.compute.amazonaws.com:1883";
-  public static final String MQTT_BROKER_TLS_STRING = "ssl://ec2-18-221-127-99.us-east-2.compute.amazonaws.com:8883";
+  public static final String MQTT_BROKER_STRING = "tcp://%s:1883";
+  public static final String MQTT_BROKER_TLS_STRING = "ssl://%s:8883";
   public static final String MQTT_CA_FILE = "/home/ubuntu/ca.crt";
   public static final String MQTT_TLS_VERSION = "TLSv1.2";
   public static final String MQTT_CLIENT_ID = "iot4pwc";
