05c1328639828527be301116b4f64f1141e43bba
Eddie_Mac <edward.wuxianru@gmail.com>
2017-10-26 22:34:31 -0400

    merged with lydia's and Tarun's branch

--- source/src/main/java/com/iot4pwc/verticles/ActuatorController.java
+++ source/src/main/java/com/iot4pwc/verticles/ActuatorController.java
@@ -1,6 +1,5 @@
 package com.iot4pwc.verticles;
 
-
 import com.iot4pwc.constants.ConstLib;
 
 import io.vertx.core.AbstractVerticle;
@@ -11,69 +10,58 @@ import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
 
 
-public class ActuatorController extends AbstractVerticle{
+public class ActuatorController extends AbstractVerticle {
 
-	@Override
-	public void start() {
-		EventBus eb = vertx.eventBus();
-		//message in certain json format: {app_id:app_id, sensor_id:sensor_id, action_id:action_id}
-		eb.consumer(ConstLib.ACTUATOR_ADDRESS, message -> {
-			System.out.println(ActuatorController.class.getName()+" : got message [" + message.body() + "]");
-			JsonObject command = new JsonObject((String) message.body());
-			JsonObject authentication = new JsonObject();
-			//String app = command.getString("app_id");
-			String action = command.getString("action_id");
-			authentication.put(ConstLib.PAYLOAD_FIELD_APP_ID, command.getString("app_id"));
-			authentication.put(ConstLib.PAYLOAD_FIELD_ACTION_ID, command.getString("action_id"));
-			eb.send(ConstLib.APP_AUTHENTICATOR_ADDRESS, authentication.toString(), auth ->{
-				System.out.println(ActuatorController.class.getName()+" : message send to authenticator");
-				if(auth.succeeded()){
-					System.out.println(ActuatorController.class.getName()+" : got message from authenticator " + auth.result().body());
-					if((boolean) auth.result().body()){
-						//if(sendRequest(command)){
-						message.reply("Success");
-						//}
-					}else{
-						message.reply("Failed");
-					}
-				}else{
-					message.reply("Failed");
-				}
-			});
-		});
+  @Override
+  public void start() {
+    EventBus eb = vertx.eventBus();
 
-	}
+    //message in certain json format: {app_id:app_id, sensor_id:sensor_id, action_id:action_id}
+    eb.consumer(ConstLib.ACTUATOR_ADDRESS, message -> {
+      System.out.println(ActuatorController.class.getName() + " : got message [" + message.body() + "]");
+      JsonObject command = new JsonObject((String) message.body());
+      JsonObject authentication = new JsonObject();
 
-	@Override
-	public void stop() {
+      String actionId = command.getString("action_id");
+      String appId = command.getString("app_id");
+      authentication.put(ConstLib.PAYLOAD_FIELD_APP_ID, appId);
+      authentication.put(ConstLib.PAYLOAD_FIELD_ACTION_ID, actionId);
 
-	}
-	
-	
-	private boolean sendRequest(JsonObject command){
-		//this part is note tested, not called as well
-		WebClient client = WebClient.create(vertx);
-		
-		String tokens = "";
-		client
-		  .get(443, "cmu.udoo.cloud", "/ext/sensors/write/"+":gatewayId/:deviceId/:sensorType/:sensorId/:pin/:value")
-		  .putHeader("Authorization", "JWT "+tokens)
-		  .send(ar -> {
-		    if (ar.succeeded()) {
-		      // Obtain response
-		      HttpResponse<Buffer> response = ar.result();
-		      System.out.println("Received response with status code" + response.statusCode());
-		    } else {
-		      System.out.println("Something went wrong " + ar.cause().getMessage());
-		    }
-		  });
-		
-		return false;
-		
-	}
-	
-	
+      eb.send(ConstLib.APP_AUTHENTICATOR_ADDRESS, authentication.toString(), auth -> {
+        System.out.println(ActuatorController.class.getName() + " : message send to authenticator");
+        if (auth.succeeded()) {
+          System.out.println(ActuatorController.class.getName() + " : got message from authenticator " + auth.result().body());
+          if ((boolean) auth.result().body()) {
+            message.reply("Success");
+          } else {
+            message.reply("Failed");
+          }
+        } else {
+          message.reply("Failed");
+        }
+      });
+    });
 
+  }
 
+  // TODO: test this method
+  private boolean sendRequest(JsonObject command) {
+    WebClient client = WebClient.create(vertx);
 
+    String tokens = "";
+    client
+      .get(443, "cmu.udoo.cloud", "/ext/sensors/write/" + ":gatewayId/:deviceId/:sensorType/:sensorId/:pin/:value")
+      .putHeader("Authorization", "JWT " + tokens)
+      .send(ar -> {
+        if (ar.succeeded()) {
+          // Obtain response
+          HttpResponse<Buffer> response = ar.result();
+          System.out.println("Received response with status code" + response.statusCode());
+        } else {
+          System.out.println("Something went wrong " + ar.cause().getMessage());
+        }
+      });
+
+    return false;
+  }
 }
--- source/src/main/java/com/iot4pwc/verticles/RESTfulDBService.java
+++ source/src/main/java/com/iot4pwc/verticles/RESTfulDBService.java
@@ -6,9 +6,7 @@ import com.iot4pwc.components.helpers.DBHelper;
 import com.iot4pwc.constants.ConstLib;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.WorkerExecutor;
 import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.eventbus.Message;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
@@ -18,51 +16,68 @@ import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 
 public class RESTfulDBService extends AbstractVerticle {
-	@Override
-	public void start(){
-		Router router = Router.router(vertx);
-		System.out.println(RESTfulDBService.class.getName()+" : Initializing RESTful service running on port 8443");
-		/**
-		 * GET /data?topic=topic&start=starttime&end=endtime&limit=limit  -- topic related data
-		 * GET /data/getSensor?limit=limit                       		  -- all sensor installed
-		 * GET /data/getLocation?location=location&limit=limit                        -- all sensor installed in certain location
-		 * POST /data  {sensorid:sensorid, action:command}
-		 */
-		router.route().handler(BodyHandler.create());
-		router.get("/data").handler(this::getSensorHistory);
-		router.get("/data/getSensor").handler(this::getInstalledSensor);
-		router.get("/data/getLocation").handler(this::getLocInfo);
-		router.post("/actuate").handler(this::actuationCommand);
+  @Override
+  public void start() {
+    Router router = Router.router(vertx);
+    /**
+     * GET /data?topic=topic&start=starttime&end=endtime&limit=limit  -- topic related data
+     * GET /data/getSensor?limit=limit                       		  -- all sensor installed
+     * GET /data/getLocation?location=location&limit=limit                        -- all sensor installed in certain location
+     * POST /data  {sensorid:sensorid, action:command}
+     */
+    router.route().handler(BodyHandler.create());
+    router.get("/data").handler(this::getSensorHistory);
+    router.get("/data/getSensor").handler(this::getInstalledSensor);
+    router.get("/data/getLocation").handler(this::getLocInfo);
+    router.post("/actuate").handler(this::actuationCommand);
 
-		vertx.createHttpServer(new HttpServerOptions().setSsl(true).setPemKeyCertOptions(new PemKeyCertOptions().setKeyPath(System.getenv("PRIVATE_KEY_PATH")).setCertPath(System.getenv("CERTIFICATE_PATH")))).requestHandler(router::accept).listen(8443);
-		//vertx.createHttpServer().requestHandler(router::accept).listen(8080);
-		System.out.println(RESTfulDBService.class.getName()+" : RESTful service running on port 8443");
+    vertx.createHttpServer(
+      new HttpServerOptions()
+        .setSsl(true)
+        .setPemKeyCertOptions(
+          new PemKeyCertOptions()
+            .setKeyPath(System.getenv("PRIVATE_KEY_PATH"))
+            .setCertPath(System.getenv("CERTIFICATE_PATH"))
+        )
+    ).requestHandler(router::accept).listen(8443);
+    System.out.println(RESTfulDBService.class.getName() + " : RESTful service running on port 8443");
+  }
 
-	}
+  private void getSensorHistory(RoutingContext routingContext) {
+    System.out.println(RESTfulDBService.class.getName() + " : GET " + routingContext.request().uri());
+    String topic = routingContext.request().getParam("topic");
+    String start = routingContext.request().getParam("start");
+    String end = routingContext.request().getParam("end");
+    String limitStr = routingContext.request().getParam("limit");
 
-	private void getSensorHistory(RoutingContext routingContext) {
-		System.out.println(RESTfulDBService.class.getName()+" : GET " + routingContext.request().uri());
-		String topic = routingContext.request().getParam("topic");
-		String start = routingContext.request().getParam("start");
-		String end = routingContext.request().getParam("end");
-		String limitStr = routingContext.request().getParam("limit");
-
-    String query = "";
+    String query;
     int limit = limitStr == null ? 100 : Integer.valueOf(limitStr.trim());
 
     List<JsonObject> result;
-    if(topic == null || start == null){
+    if (topic == null || start == null) {
       routingContext.response()
         .putHeader("content-type", "application/json; charset=utf-8")
         .setStatusCode(400)
         .end();
       return;
-    }else if(end == null){
-      query = "select * from sensor_history where recorded_time >= STR_TO_DATE('" + start + "', '%Y-%m-%d %H:%i:%S') and sensor_id in (select distinct sensor_id from sensor_topic_map where topic = '"+ topic + "') order by recorded_time desc limit "+ limit +";";
+    } else if (end == null) {
+      query = "select * from sensor_history " +
+        "where recorded_time >= STR_TO_DATE('" + start + "', '%Y-%m-%d %H:%i:%S') " +
+        "and sensor_id in " +
+        "(select distinct sensor_id from sensor_topic_map " +
+        "where topic = '" + topic + "') order by recorded_time desc " +
+        "limit " + limit + ";";
       System.out.println(query);
       result = DBHelper.getInstance(ConstLib.SERVICE_PLATFORM).select(query);
-    }else {
-      query = "select * from sensor_history where recorded_time between STR_TO_DATE('" + start + "', '%Y-%m-%d %H:%i:%S') and STR_TO_DATE('" + end + "', '%Y-%m-%d %H:%i:%S') and sensor_id in (select distinct sensor_id from sensor_topic_map where topic = '" + topic + "') order by recorded_time desc limit "+ limit +";";
+    } else {
+      query = "select * from sensor_history " +
+        "where recorded_time between STR_TO_DATE('" + start + "', '%Y-%m-%d %H:%i:%S') " +
+        "and STR_TO_DATE('" + end + "', '%Y-%m-%d %H:%i:%S') " +
+        "and sensor_id in " +
+        "(select distinct sensor_id from sensor_topic_map " +
+        "where topic = '" + topic + "') " +
+        "order by recorded_time desc " +
+        "limit " + limit + ";";
       System.out.println(query);
       result = DBHelper.getInstance(ConstLib.SERVICE_PLATFORM).select(query);
     }
@@ -72,82 +87,77 @@ public class RESTfulDBService extends AbstractVerticle {
       .putHeader("content-type", "application/json; charset=utf-8")
       .setStatusCode(200)
       .end(obj.encodePrettily());
-	}
+  }
 
-	private void getInstalledSensor(RoutingContext routingContext){
+  private void getInstalledSensor(RoutingContext routingContext) {
     String limitStr = routingContext.request().getParam("limit");
     int limit = limitStr == null ? 100 : Integer.valueOf(limitStr.trim());
-		System.out.println(RESTfulDBService.class.getName()+" : GET " + routingContext.request().uri());
-		List<JsonObject> result = DBHelper.getInstance(ConstLib.SERVICE_PLATFORM).select("select * from sensor limit "+ limit + ";");
-		JsonArray arr = new JsonArray(result);
-		JsonObject obj = new JsonObject().put("result", arr);
-		routingContext.response()
-			.putHeader("content-type", "application/json; charset=utf-8")
-			.setStatusCode(200)
-			.end(obj.encodePrettily());
-	}
-
-	private void getLocInfo(RoutingContext routingContext){
-		System.out.println(RESTfulDBService.class.getName()+" : GET " + routingContext.request().uri());
-		String location = routingContext.request().getParam("location");
-    String limitStr = routingContext.request().getParam("limit");
-    int limit = limitStr == null ? 100 : Integer.valueOf(limitStr.trim());
-		List<JsonObject> result = DBHelper
-			.getInstance(ConstLib.SERVICE_PLATFORM)
-			.select("select * from sensor where install_loc like '%" + location + "%' limit "+ limit +";");
+    System.out.println(RESTfulDBService.class.getName() + " : GET " + routingContext.request().uri());
+    List<JsonObject> result = DBHelper.getInstance(ConstLib.SERVICE_PLATFORM).select("select * from sensor limit " + limit + ";");
     JsonArray arr = new JsonArray(result);
     JsonObject obj = new JsonObject().put("result", arr);
     routingContext.response()
       .putHeader("content-type", "application/json; charset=utf-8")
       .setStatusCode(200)
       .end(obj.encodePrettily());
-	}
+  }
 
-	private void actuationCommand(RoutingContext routingContext){
-		System.out.println(RESTfulDBService.class.getName()+" : POST " + routingContext.request().uri());
-		JsonObject body = routingContext.getBodyAsJson();
-		if(body.isEmpty() || !body.containsKey("sensor_id") || !body.containsKey("action_id") || !body.containsKey("app_id")){
-			routingContext.response()
-			.putHeader("content-type", "application/json; charset=utf-8")
-			.setStatusCode(400)
-			.end();
-			return;
-		}
-		
-		//System.out.println("Response status at check point 1: " + routingContext.response().ended());
-		System.out.println(RESTfulDBService.class.getName()+" : sending to acutator");
-		
-		EventBus eb = vertx.eventBus();
-		eb.send(ConstLib.ACTUATOR_ADDRESS, body.toString(), ar -> {
-			System.out.println(RESTfulDBService.class.getName()+" : message send to actuator");
-			if (ar.succeeded()) {
-				System.out.println(RESTfulDBService.class.getName()+ar.result().body());
-				String result = (String) ar.result().body();
-				if(result.equals("Success")){
-					System.out.println(RESTfulDBService.class.getName()+" : Action request ["+ body.getString("action_id") + "] on #" + body.getString("sensor_id") + " processed");
-					//System.out.println("Response status at check point 2: " + routingContext.response().ended());
-					routingContext.response()
-					.putHeader("content-type", "application/json; charset=utf-8")
-					.setStatusCode(200)
-					.end("Action request processed");
-					return;
-				}
-				System.out.println(RESTfulDBService.class.getName()+" : Action request ["+ body.getString("action_id") + "] on #" + body.getString("sensor_id") + " failed due to " + ar.result().body());
-				routingContext.response()
-				.putHeader("content-type", "application/json; charset=utf-8")
-				.setStatusCode(200)
-				.end("Action request failed");
-				return;
-			}else{
-				System.out.println(RESTfulDBService.class.getName()+" : Action request ["+ body.getString("action_id") + "] on #" + body.getString("sensor_id") + " failed due to " + ar.result().body());
-				routingContext.response()
-				.putHeader("content-type", "application/json; charset=utf-8")
-				.setStatusCode(500)
-				.end("Action request failed");
-				return;
-			}
-		});
-		
-		//System.out.println("Response status at check point 3: " + routingContext.response().ended());
-	}
+  private void getLocInfo(RoutingContext routingContext) {
+    System.out.println(RESTfulDBService.class.getName() + " : GET " + routingContext.request().uri());
+    String location = routingContext.request().getParam("location");
+    String limitStr = routingContext.request().getParam("limit");
+    int limit = limitStr == null ? 100 : Integer.valueOf(limitStr.trim());
+    List<JsonObject> result = DBHelper
+      .getInstance(ConstLib.SERVICE_PLATFORM)
+      .select("select * from sensor where install_loc like '%" + location + "%' limit " + limit + ";");
+    JsonArray arr = new JsonArray(result);
+    JsonObject obj = new JsonObject().put("result", arr);
+    routingContext.response()
+      .putHeader("content-type", "application/json; charset=utf-8")
+      .setStatusCode(200)
+      .end(obj.encodePrettily());
+  }
+
+  private void actuationCommand(RoutingContext routingContext) {
+    System.out.println(RESTfulDBService.class.getName() + " : POST " + routingContext.request().uri());
+    JsonObject body = routingContext.getBodyAsJson();
+    if (body.isEmpty() || !body.containsKey("sensor_id") || !body.containsKey("action_id") || !body.containsKey("app_id")) {
+      routingContext.response()
+        .putHeader("content-type", "application/json; charset=utf-8")
+        .setStatusCode(400)
+        .end();
+      return;
+    }
+
+    System.out.println(RESTfulDBService.class.getName() + " : sending to acutator");
+
+    EventBus eb = vertx.eventBus();
+    eb.send(ConstLib.ACTUATOR_ADDRESS, body.toString(), ar -> {
+      System.out.println(RESTfulDBService.class.getName() + " : message send to actuator");
+      if (ar.succeeded()) {
+        String result = (String) ar.result().body();
+        if (result.equals("Success")) {
+          System.out.println(RESTfulDBService.class.getName() + " : Action request [" + body.getString("action_id") + "] on #" + body.getString("sensor_id") + " processed");
+          routingContext.response()
+            .putHeader("content-type", "application/json; charset=utf-8")
+            .setStatusCode(200)
+            .end("Action request processed");
+          return;
+        }
+        System.out.println(RESTfulDBService.class.getName() + " : Action request [" + body.getString("action_id") + "] on #" + body.getString("sensor_id") + " failed due to " + ar.result().body());
+        routingContext.response()
+          .putHeader("content-type", "application/json; charset=utf-8")
+          .setStatusCode(200)
+          .end("Action request failed");
+        return;
+      } else {
+        System.out.println(RESTfulDBService.class.getName() + " : Action request [" + body.getString("action_id") + "] on #" + body.getString("sensor_id") + " failed due to " + ar.result().body());
+        routingContext.response()
+          .putHeader("content-type", "application/json; charset=utf-8")
+          .setStatusCode(500)
+          .end("Action request failed");
+        return;
+      }
+    });
+  }
 }
