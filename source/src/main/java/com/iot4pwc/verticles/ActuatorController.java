4eb152372623e0e7a44aad573d4f869cf68f08d1
lidyxfour <lidyxfour@163.com>
2017-11-02 09:53:45 -0400

    IOT-131 code style revised

--- source/src/main/java/com/iot4pwc/verticles/ActuatorController.java
+++ source/src/main/java/com/iot4pwc/verticles/ActuatorController.java
@@ -15,78 +15,76 @@ import io.vertx.ext.web.client.WebClient;
 
 
 public class ActuatorController extends AbstractVerticle {
-  Logger logger = LogManager.getLogger(ActuatorController.class);
-  Logger actuatorLogger = LogManager.getLogger("com.iot4pwc.actuator.controller");
-  private DBHelper dbHelper;
-  @Override
-  public void start() {
-    EventBus eb = vertx.eventBus();
+	Logger logger = LogManager.getLogger(ActuatorController.class);
+	Logger actuatorLogger = LogManager.getLogger("com.iot4pwc.actuator.controller");
+	private DBHelper dbHelper;
+	@Override
+	public void start() {
+		EventBus eb = vertx.eventBus();
 
-    vertx.executeBlocking(future -> {
-      dbHelper = DBHelper.getInstance(ConstLib.SERVICE_PLATFORM);
-      future.complete();
-    }, response -> {
-      //message in certain json format: {app_id:app_id, sensor_id:sensor_id, action_id:action_id}
-      eb.consumer(ConstLib.ACTUATOR_ADDRESS, message -> {
-        logger.info("got message [" + message.body() + "]");
-        JsonObject command = new JsonObject((String) message.body());
-        JsonObject authentication = new JsonObject();
+		vertx.executeBlocking(future -> {
+			dbHelper = DBHelper.getInstance(ConstLib.SERVICE_PLATFORM);
+			future.complete();
+		}, response -> {
+			//message in certain json format: {app_id:app_id, sensor_id:sensor_id, action_id:action_id}
+			eb.consumer(ConstLib.ACTUATOR_ADDRESS, message -> {
+				logger.info("got message [" + message.body() + "]");
+				JsonObject command = new JsonObject((String) message.body());
+				JsonObject authentication = new JsonObject();
 
-        String actionId = command.getString("action_id");
-        String appId = command.getString("app_id");
-        authentication.put(ConstLib.PAYLOAD_FIELD_APP_ID, appId);
-        authentication.put(ConstLib.PAYLOAD_FIELD_ACTION_ID, actionId);
+				String actionId = command.getString("action_id");
+				String appId = command.getString("app_id");
+				authentication.put(ConstLib.PAYLOAD_FIELD_APP_ID, appId);
+				authentication.put(ConstLib.PAYLOAD_FIELD_ACTION_ID, actionId);
 
-      eb.send(ConstLib.APP_AUTHENTICATOR_ADDRESS, authentication.toString(), auth -> {
-        logger.info("message send to authenticator");
-        if (auth.succeeded()) {
-          logger.info("got message from authenticator " + auth.result().body());
-          if ((boolean) auth.result().body()) {
-        	actuatorLogger.info("[Authenticated] Action [" + actionId + "] on sensor #" + 
-        			command.getString("sensor_id") + " from application " + 
-        			appId);
-        	//send out request
-            message.reply("Success");
-          } else {
-        	actuatorLogger.info("[Unauthenticated] Action [" + actionId + "] on sensor #" + 
-        			command.getString("sensor_id") + " from application " + 
-        			appId);
-            message.reply("Failed");
-          }
-        } else {
-          actuatorLogger.info("[Failure] Action [" + actionId + "] on sensor #" + 
-        		  command.getString("sensor_id") + " from application " + 
-        		  appId);
-          message.reply("Failed");
-        }
-        });
-      });
-    });
+				eb.send(ConstLib.APP_AUTHENTICATOR_ADDRESS, authentication.toString(), auth -> {
+					logger.info("message send to authenticator");
+					if (auth.succeeded()) {
+						logger.info("got message from authenticator " + auth.result().body());
+						if ((boolean) auth.result().body()) {
+							actuatorLogger.info("[Authenticated] Action [" + actionId + "] on sensor #" + 
+									command.getString("sensor_id") + " from application " + 
+									appId);
+							//send out request
+							message.reply("Success");
+						} else {
+							actuatorLogger.info("[Unauthenticated] Action [" + actionId + "] on sensor #" + 
+									command.getString("sensor_id") + " from application " + 
+									appId);
+							message.reply("Failed");
+						}
+					} else {
+						actuatorLogger.info("[Failure] Action [" + actionId + "] on sensor #" + 
+								command.getString("sensor_id") + " from application " + 
+								appId);
+						message.reply("Failed");
+					}
+				});
+			});
+		});
 
-  }
+	}
 
-  @Override
-  public void stop() {
-  }
-  
-  // TODO: test this method
-  private boolean sendRequest(JsonObject command) {
-    WebClient client = WebClient.create(vertx);
+	private boolean sendRequest(JsonObject command) {
+		WebClient client = WebClient.create(vertx);
 
-    String tokens = "";
-    client
-      .get(443, "cmu.udoo.cloud", "/ext/sensors/write/" + ":gatewayId/:deviceId/:sensorType/:sensorId/:pin/:value")
-      .putHeader("Authorization", "JWT " + tokens)
-      .send(ar -> {
-        if (ar.succeeded()) {
-          // Obtain response
-          HttpResponse<Buffer> response = ar.result();
-          logger.info("Received response with status code" + response.statusCode());
-        } else {
-          logger.error("Something went wrong " + ar.cause().getMessage());
-        }
-      });
+		//http://udoo-iot-beta.cleverapps.io
+		String tokens = "";
+		//1: on   0:off
+		String action = command.getString("action_id");
+		client
+		.get(8080, "udoo-iot-beta.cleverapps.io", "/ext/sensors/write/82ccd7c9f70f23cbe570d1644f60a7293603fe95c5c51cabc6ee0de72f0df61d/ttyMCC-2125c1d4df669959/digital/13/"+action)
+		.putHeader("Authorization", "JWT " + tokens)
+		.send(ar -> {
+			if (ar.succeeded()) {
+				// Obtain response
+				HttpResponse<Buffer> response = ar.result();
+				logger.info("Received response with status code" + response.statusCode());
+			} else {
+				logger.error("Something went wrong " + ar.cause().getMessage());
+			}
+		});
 
-    return false;
-  }
+		return false;
+	}
 }
