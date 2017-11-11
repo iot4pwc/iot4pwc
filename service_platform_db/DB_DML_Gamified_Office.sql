0130d4d20aa61cd6fae7a00c77f64de71ad075ae
Tarun <tarunkhandelwal.iitr@gmail.com>
2017-11-11 15:47:29 -0500

    Add grant all statement

--- service_platform_db/DB_DML_Gamified_Office.sql
+++ service_platform_db/DB_DML_Gamified_Office.sql
@@ -1,7 +1,15 @@
 DROP DATABASE IF EXISTS gamified_office;
 CREATE DATABASE gamified_office;
 
+GRANT ALL PRIVILEGES ON gamified_office.* TO 'iot4pwc'@'%';
+
 USE gamified_office;
+
+DROP TABLE IF EXISTS participant_component_score;
+DROP TABLE IF EXISTS participant;
+DROP TABLE IF EXISTS app_user;
+DROP TABLE IF EXISTS challenge_component;
+DROP TABLE IF EXISTS challenge;
 
 CREATE TABLE challenge(
   challenge_id int(10) AUTO_INCREMENT,
@@ -13,8 +21,8 @@ CREATE TABLE challenge(
 CREATE TABLE challenge_component(
   component_id int(10) AUTO_INCREMENT,
   challenge_id int(10),
-  component_code varchar(20), /*component means what all does challenge measures. Ex- sitting and water would be two components.*/
-  component_weight int(2), /*weight out of hundred (50 means weight is 0.5)*/
+  component_code varchar(20), 
+  component_weight int(2), 
   component_desc varchar(100),
   CONSTRAINT challenge_component_pk PRIMARY KEY (component_id),
   CONSTRAINT challenge_component_fk FOREIGN KEY (challenge_id) REFERENCES challenge (challenge_id)
@@ -26,8 +34,8 @@ CREATE TABLE app_user(
   alias varchar(80),
   reg_date DATETIME,
   age int(3),
-  share_flag varchar(1), /*do you want to share your ranking info with others? If not, you will not be able to see anyone's ranking too.*/
-  compete_flag varchar(1), /*do you want to compete? If no, admin will not put you in any challenge.*/
+  share_flag varchar(1), 
+  compete_flag varchar(1), 
   CONSTRAINT app_user_pk PRIMARY KEY (email)
 );
 
@@ -35,7 +43,6 @@ CREATE TABLE participant(
   participant_id int(10) AUTO_INCREMENT,
   challenge_id int(10),
   email varchar(255),
-  /*Following scores are redundant but just saved for faster lookup*/
   total_score int(5),
   today_score int(5),
   yesterday_score int(5),
@@ -50,8 +57,7 @@ CREATE TABLE participant_component_score(
   part_comp_id int(10) AUTO_INCREMENT,
   component_id int(10),
   email varchar(255),
-  /*Store component wise score to have more info that can be shown.*/
-  total_score int(5),
+  total_score int(5),quit
   today_score int(5),
   yesterday_score int(5),
   last_week_score int(5),
