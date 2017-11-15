741a53c2211788721c4a62d800e1d9693732e767
Eddie_Mac <edward.wuxianru@gmail.com>
2017-11-15 01:37:25 -0500

    fixed mysql setup

--- scripts/mysql_setup.sh
+++ scripts/mysql_setup.sh
@@ -2,6 +2,7 @@
 
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.8-1_all.deb
 curl https://raw.githubusercontent.com/iot4pwc/iot4pwc/master/service_platform_db/DB_DML.sql > DB_DML.sql
+curl https://raw.githubusercontent.com/iot4pwc/iot4pwc/master/service_platform_db/DB_Dummy_Data.sql > DB_Dummy_Data.sql
 sudo dpkg -i mysql-apt-config*
 sudo apt-get update
 sudo apt-get install -y mysql-server
@@ -12,4 +13,4 @@ export DB_USER_PW
 # you will need to set the passwd for root, remember that pwd
 
 mysql -u root -p < DB_DML.sql -f
-mysql -u root -p < DB_Dummy_Dat.sql -f
+mysql -u root -p < DB_Dummy_Data.sql -f
