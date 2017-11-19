d93d7b4d1561cbdb79a326c123cebdf554a08de9
yanw <yanw4@andrew.cmu.edu>
2017-11-18 21:01:24 -0500

    IOT-155 resolved interval issue

--- source/src/main/java/com/iot4pwc/components/helpers/RFIDDataPoller.java
+++ source/src/main/java/com/iot4pwc/components/helpers/RFIDDataPoller.java
@@ -2,8 +2,8 @@ package com.iot4pwc.components.helpers;
 import com.iot4pwc.constants.ConstLib;
 
 public class RFIDDataPoller extends DataPollerHelper {
-	private static final String query = "select gateway_id, device_id, sensor_type, sensor_id from sensor where sensor_type="+"'"+ConstLib.RFID_SENSOR_TYPE+"';";
-	private static final int frequency = 300000;
+	private static final String query = "select sensor_pk_id,gateway_id, device_id, sensor_type, sensor_id from sensor where sensor_type="+"'"+ConstLib.RFID_SENSOR_TYPE+"';";
+	private static final int frequency = 60000;
 	private static RFIDDataPoller pollerInstance;
 
 	public static RFIDDataPoller getInstance() {
