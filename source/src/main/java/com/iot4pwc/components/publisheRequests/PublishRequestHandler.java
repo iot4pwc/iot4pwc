008aa0096986be5c3b79cd6d4d1e14a23a9cf486
iot4pwc <32587329+iot4pwc@users.noreply.github.com>
2017-11-26 23:02:16 -0500

    Update PublishRequestHandler.java

--- source/src/main/java/com/iot4pwc/components/publisheRequests/PublishRequestHandler.java
+++ source/src/main/java/com/iot4pwc/components/publisheRequests/PublishRequestHandler.java
@@ -2,6 +2,15 @@ package com.iot4pwc.components.publisheRequests;
 
 import com.iot4pwc.components.helpers.MqttHelper;
 
+/**
+ * A handler class that handles publishing message to MQTT
+ * Author: Xianru Wu
+ */
 public interface PublishRequestHandler {
+  /**
+   * Publish a message to MQTT
+   * @params
+   * mqttHelper: MqttHelper, the MQTT helper instance
+   */    
   void handlePublish(MqttHelper mqttHelper);
 }
